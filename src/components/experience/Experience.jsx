2768302b058b9c5a3b44ad2f14aff3be329f4799
unknown <tonyseckar@gmail.com>
2022-06-06 18:45:21 -0700

    exp add

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,8 +1,8 @@
-import React from 'react'
-import './experience.css'
-import { Carousel } from 'react-bootstrap'
-import Sample from '../../assets/SampleVid.mp4'
-import ReactPlayer from 'react-player'
+import React from "react";
+import "./experience.css";
+import Sample from "../../assets/SampleVid.mp4";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from "react-responsive-carousel";
 
 const Experience = () => {
   const videoProperites = [
@@ -11,43 +11,38 @@ const Experience = () => {
       title: "Video 1",
       src: Sample,
       credit: "Video by Shiro",
-    } ,
+    },
     {
       id: 2,
       title: "Video 2",
       src: Sample,
       credit: "Video by Shiro 2",
-    } ,
+    },
     {
       id: 3,
       title: "Video 3",
       src: Sample,
       credit: "Video by Shiro 3",
-    } ,
-  ]
+    },
+  ];
 
   return (
-    <section id='experience'>
-      <Carousel>
-        {videoProperites.map((videoObj) => {
-          return (
-            <Carousel.Item key={videoObj.id}>
-              <ReactPlayer 
-                url = {videoObj.src}
-                width="100%"
-                pip={true}
-                playing={true}
-              />
-              <Carousel.Caption>
-                  <h3 id = "h3">{videoObj.title}</h3>
-                  <p>{videoObj.credit}</p>
-              </Carousel.Caption>
-            </Carousel.Item>
-          )
-        })}
+    <section id="experience">
+      <h1>My work</h1>
+      <Carousel className="main">
+        {videoProperites
+          ? videoProperites.map((item) => (
+              <div className="video">
+                <video src={item.src} controls>
+                  Your browser does not support the video tag.
+                </video>
+                <p className="legend">{item.title}</p>
+              </div>
+            ))
+          : null}
       </Carousel>
     </section>
-  )
-}
+  );
+};
 
-export default Experience
+export default Experience;
--- src/components/service/Service.jsx
+++ src/components/service/Service.jsx
@@ -1,10 +1,81 @@
 import React from 'react'
 import './service.css'
+import {BsCheck} from 'react-icons/bs'
 
 const Service = () => {
   return (
     <section id='service'>
-      Service
+      <h1>My Commission Info!</h1>
+
+      <div className="container service__container">
+        <article className="service">
+          <div className="service__head">
+            <h3>Rigging Commission</h3>
+          </div>
+
+          <ul className='service__list'>
+            <li>
+              <BsCheck className='service__list-icon' />
+              <p>Something something something Bust</p>
+            </li>
+            <li>
+              <BsCheck className='service__list-icon' />
+              <p>Something something something half body</p>
+            </li>
+            <li>
+              <BsCheck className='service__list-icon' />
+              <p>Something something something full body</p>
+            </li>
+          </ul>
+        </article>
+        {/*End of Rigging comm*/}
+
+
+        <article className="service">
+          <div className="service__head">
+            <h3>Art Commission</h3>
+          </div>
+
+          <ul className='service__list'>
+            <li>
+              <BsCheck className='service__list-icon' />
+              <p>Something something something Bust</p>
+            </li>
+            <li>
+              <BsCheck className='service__list-icon' />
+              <p>Something something something half body</p>
+            </li>
+            <li>
+              <BsCheck className='service__list-icon' />
+              <p>Something something something full body</p>
+            </li>
+          </ul>
+        </article>
+        {/*End of Art comm*/}       
+        
+         <article className="service">
+          <div className="service__head">
+            <h3>Art + Rig Commission</h3>
+          </div>
+
+          <ul className='service__list'>
+            <li>
+              <BsCheck className='service__list-icon' />
+              <p>Something something something Bust</p>
+            </li>
+            <li>
+              <BsCheck className='service__list-icon' />
+              <p>Something something something half body</p>
+            </li>
+            <li>
+              <BsCheck className='service__list-icon' />
+              <p>Something something something full body</p>
+            </li>
+          </ul>
+        </article>
+        {/*End of Art + Rig comm*/}
+
+      </div>
     </section>
   )
 }
